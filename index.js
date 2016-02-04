c203b4c2001fcadcf008d02d099d5856cdb231ae
Michael Hirsch <michael@hirschstudios.com>
2016-02-04 10:10:57 -0600

    Update from JSX to JS

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+'use strict';
+
 var React = require('react');
 var shared = require('./shared');
 
 var Initicon = React.createClass({
+  displayName: 'Initicon',
+
   propTypes: {
     text: React.PropTypes.string.isRequired,
     size: React.PropTypes.any.isRequired,
@@ -11,24 +15,28 @@ var Initicon = React.createClass({
     saturation: React.PropTypes.string,
     brightness: React.PropTypes.string
   },
-  getDefaultProps: function() {
-    return shared.defaultProps
+  getDefaultProps: function getDefaultProps() {
+    return shared.defaultProps;
   },
-  render: function() {
+  render: function render() {
     if (this.props.color) {
       var backgroundColor = this.props.color;
     } else {
       var backgroundColor = shared.getBackgroundColor(this.props.seed, this.props.saturation, this.props.brightness);
     }
-    var fontSize = this.props.single ? (this.props.size)/1.7 : (this.props.size-5)/2;
-    return (
-      <div style={{ backgroundColor,
-                    height: this.props.size + "px",
-                    width: this.props.size + "px",
-                    fontSize: fontSize + "px",
-                    lineHeight: this.props.size + "px" }}>
-        {shared.getInitials(this.props.text, this.props.single)}
-      </div>
+    var fontSize = this.props.single ? this.props.size / 1.7 : (this.props.size - 5) / 2;
+    return React.createElement(
+      'div',
+      { style: { backgroundColor: backgroundColor,
+          height: this.props.size + "px",
+          width: this.props.size + "px",
+          fontSize: fontSize + "px",
+          lineHeight: this.props.size + "px",
+          borderRadius: '50%',
+          display: 'inline-block',
+          textAlign: 'center',
+          color: '#ffffff' } },
+      shared.getInitials(this.props.text, this.props.single)
     );
   }
 });
